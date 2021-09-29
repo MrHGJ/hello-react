9e433b7ed100e7c3490ff3f09f04893a8ead9cea
MrHGJ <1241261212@qq.com>
2021-09-29 16:50:30 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+// 解决代码质量问题
+// 它能在我们编写代码时就检测出程序可能出现的隐性BUG
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'standard',
+    'prettier',
+    // prettier和eslint冲突解决
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100,
+        semi: false,
+        singleQuote: true,
+      },
+    ],
+    'no-use-before-define': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/no-explicit-any': 0,
+
+    'react/prop-types': 0,
+  },
+  ignorePatterns: ['!.*.js'],
+}
