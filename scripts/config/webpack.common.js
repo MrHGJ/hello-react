4159e54116caca8e8c6211ef8d46253940dcba2c
hugangjun <hugangjun@xiaomi.com>
2021-09-30 15:12:03 +0800

    feat: 添加webpack基础配置

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -1,12 +1,14 @@
 const path = require('path')
+const { isDev, PROJECT_PATH } = require('../constants')
 module.exports = {
   // 定义了入口文件路径，其属性名 app 表示引入文件的名字。
   entry: {
-    app: path.resolve(__dirname, '../../src/app.js'),
+    app: path.resolve(PROJECT_PATH, './src/app.js'),
   },
   // 定义了编译打包之后的文件名以及所在路径。
   output: {
-    filename: 'js/[name].[hash:8].js',
-    path: path.resolve(__dirname, '../../dist'),
+    filename: `js/[name]${isDev ? '' : '.[hash:8]'}.js`,
+    // path.resolve: 可以将路径或者路径片段解析成绝对路径。
+    path: path.resolve(PROJECT_PATH, './dist'),
   },
 }
