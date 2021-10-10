ada5a8df08147bb156d7c5b0eeb1ab08312a9df2
hugangjun <hugangjun@xiaomi.com>
2021-10-10 11:58:55 +0800

    feat: 添加webpack配置，使页面能够展示

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { isDev, PROJECT_PATH } = require('../constants')
 module.exports = {
   // 定义了入口文件路径，其属性名 app 表示引入文件的名字。
@@ -11,4 +12,27 @@ module.exports = {
     // path.resolve: 可以将路径或者路径片段解析成绝对路径。
     path: path.resolve(PROJECT_PATH, './dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(PROJECT_PATH, './public/index.html'),
+      filename: 'index.html',
+      cache: false, // 特别重要：防止之后使用v6版本 copy-webpack-plugin 时代码修改一刷新页面为空问题。
+      minify: isDev
+        ? false
+        : {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            removeComments: true,
+            collapseBooleanAttributes: true,
+            collapseInlineTagWhitespace: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            minifyCSS: true,
+            minifyJS: true,
+            minifyURLs: true,
+            useShortDoctype: true,
+          },
+    }),
+  ],
 }
--- scripts/constants.js
+++ scripts/constants.js
@@ -7,8 +7,13 @@ const PROJECT_NAME = path.parse(PROJECT_PATH).name
 
 const isDev = process.env.NODE_ENV !== 'production'
 
+const SERVER_HOST = '127.0.0.1'
+const SERVER_PORT = 9000
+
 module.exports = {
   PROJECT_PATH,
   PROJECT_NAME,
   isDev,
+  SERVER_HOST,
+  SERVER_PORT,
 }
